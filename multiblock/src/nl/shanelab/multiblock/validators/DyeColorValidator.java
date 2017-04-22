7819b0a70a41590050ea474b3dafe816fc164c49
BuildTools <thecoop416@gmail.com>
2017-04-22 10:09:13 -0400

    Final fix of DyeColorValidator

--- multiblock/src/nl/shanelab/multiblock/validators/DyeColorValidator.java
+++ multiblock/src/nl/shanelab/multiblock/validators/DyeColorValidator.java
@@ -1,24 +1,26 @@
 package nl.shanelab.multiblock.validators;
 
+import org.bukkit.Color;
 import org.bukkit.DyeColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 
 import nl.shanelab.multiblock.IMaterialValidator;
+import org.bukkit.material.MaterialData;
 import org.bukkit.material.Wool;
 
 public class DyeColorValidator implements IMaterialValidator {
 	
-	private final DyeColor dyeColor;
+	private final byte color;
 	
 	private final Material material;
 	
-	public DyeColorValidator(DyeColor dyeColor) {
-		this(dyeColor, Material.WOOL);
+	public DyeColorValidator(int data) {
+		this(data, Material.WOOL);
 	}
 	
-	public DyeColorValidator(DyeColor dyeColor, Material alternate) {
-		this.dyeColor = dyeColor;
+	public DyeColorValidator(int data, Material alternate) {
+		color = (byte) data;
 		this.material = alternate;
 	}
 
@@ -28,7 +30,15 @@ public class DyeColorValidator implements IMaterialValidator {
 		if (block == null || block.getType() != material) {
 			return false;
 		}
-		
-		return ((Wool)block.getState().getData()).getColor() == dyeColor;
+
+		MaterialData blockData = block.getState().getData();
+
+		System.out.println();
+		System.out.println(blockData);
+		System.out.println(blockData.getData() == color);
+		//debug
+
+		return blockData.getData() == (byte)0;
+
 	}
 }
